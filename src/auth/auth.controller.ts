f767603e1a9ecf0c5389b7de2a6da111fa69b3e3
emanuelarevalomartinez <emanuelarevalomartinez@gmail.com>
2026-06-28 18:37:24 -0400

    creacion de decoradores personalizados para verificar presencia del usuario y del token a la hora de verificar el token en el microservicio de auth, creacion de guard en auth para verificar presencia de token asi como llamar al microservicio de auth verify y generar un nuevo token y traer lso datos del usuario, creacion de interfaz para validar cuales son los datos de usuario esperados para validar

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,39 @@
-import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config';
 import { LoginUserDto, RegisterUserDto } from './dto';
+import { AuthGuard } from './guards/auth.guard';
+import { Token, User } from './decorators';
+import type { CurrentUser } from './interfaces/current-user.interface';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('register')
-  registerUser( @Body() registerUserDto: RegisterUserDto ) {
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
     return this.client.send('auth.register.user', registerUserDto);
   }
 
   @Post('login')
-  loginUser( @Body() loginUserDto: LoginUserDto ) {
+  loginUser(@Body() loginUserDto: LoginUserDto) {
     return this.client.send('auth.login.user', loginUserDto);
   }
 
+  @UseGuards(AuthGuard)
   @Get('verify')
-  verifyToken() {
-    return this.client.send('auth.verify.user', {});
+  verifyToken(@User() user: CurrentUser, @Token() token: string) {
+    return {
+      user,
+      token,
+    };
   }
 }
