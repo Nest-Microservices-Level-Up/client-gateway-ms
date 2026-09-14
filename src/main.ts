6bf7f7c7a24de4fadb0f289da2cbb5d5bcec6741
emanuelarevalomartinez <emanuelarevalomartinez@gmail.com>
2026-09-14 00:41:31 -0400

    añadida la documentacion de la api mediante la libreria de swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
 import { RpcCustomExceptionFilter } from './common';
 import { NatsExceptionFilter } from './common/exceptions/nats-custom-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
 
@@ -23,6 +24,26 @@ async function bootstrap() {
   app.useGlobalFilters( new RpcCustomExceptionFilter());
 
   app.useGlobalFilters(new NatsExceptionFilter());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('E-Commerce Microservices API')
+    .setDescription(
+      'REST API Gateway for the E-Commerce Microservices Platform.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(
+    app,
+    swaggerConfig,
+  );
+
+  SwaggerModule.setup(
+    'docs',
+    app,
+    swaggerDocument,
+  );
 
   await app.listen(envs.port);
   logger.log(`Gateway running on port ${envs.port}`);
